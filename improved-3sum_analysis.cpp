5152edd96c1c40e35560b2f8af0402858cb65f96
Lukas Gucci <lukasgucci@ucsb.edu>
2025-04-11 21:37:37 -0700

    improved 3sum with chrono timing

--- improved-3sum_analysis.cpp
+++ improved-3sum_analysis.cpp
@@ -35,3 +35,33 @@ vector<vector<int>> threeSum(vector<int>& nums) {
     }
     return answer;
 }
+
+vector<int> tests(int n){
+    vector<int> nums;
+
+    int a = rand() % 100 + 1;
+    int b = rand() % 100 + 1;
+    nums.push_back(-a);
+    nums.push_back(-b);
+    nums.push_back(a + b);
+
+    while (nums.size() < n){
+        nums.push_back(rand() % 400 - 200);
+    }
+
+    return nums;
+}
+
+int main(){
+    vector<int> lengthNumbers = {400,800,1600,3200,8000};
+    for(int i =0; i < 5; i++){
+        vector<int> testCase = tests(lengthNumbers.at(i));
+        auto start = chrono::high_resolution_clock::now();
+        vector<vector<int>> result = threeSum(testCase);
+        auto end = chrono::high_resolution_clock::now();
+        double time_ms = chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0;
+        cout << "n = " << lengthNumbers.at(i) << " → Time: " << time_ms << " ms, Found: " << result.size() << " triplets" << endl;
+    }
+    return 0;
+
+}
